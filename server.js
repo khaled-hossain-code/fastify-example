656a2bf434340214d6d13678f0bc8d704c7d5f99
khaled hossain <khaled.hossain@appnap.io>
2023-05-09 10:03:24 +0600

    initialize fastapi

---
+++ server.js
@@ -0,0 +1,18 @@
+const fastify = require("fastify")({
+  logger: true,
+});
+
+fastify.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 4000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
