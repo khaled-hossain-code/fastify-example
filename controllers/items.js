3c4c93febb3daff093b120925554f38fc5e4a1fd
khaled hossain <khaled.hossain@appnap.io>
2023-05-09 18:59:47 +0600

    Add simple crud json mock server

--- controllers/items.js
+++ controllers/items.js
@@ -1,35 +1,18 @@
-const _ = require("lodash");
-const { v4: uuidv4 } = require("uuid");
-const ItemModel = require("../model/itemModel");
-const itemObj = new ItemModel();
+const { getAllItemData, getItemData, postItemData } = require("../data/items");
 
 const getAllItem = async (request, reply) => {
-  return itemObj.data;
+  return getAllItemData;
 };
 
 const getItem = async (request, reply) => {
-  const { item_id } = request.params;
-
-  return _.find(itemObj.data, { id: item_id });
+  return getItemData;
 };
 
 const postItem = async (request, reply) => {
-  const { name } = request.body;
-  const newItem = {
-    id: uuidv4(),
-    name,
-  };
-
-  itemObj.add(newItem);
-
-  return reply.code(201).send(newItem);
+  return reply.code(201).send(postItemData);
 };
 
 const deleteItem = async (request, reply) => {
-  const { item_id } = request.params;
-
-  itemObj.delete(item_id);
-
   reply.send({
     message: "item is deleted",
   });
