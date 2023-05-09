be839e082f4c542f1208b612b356c53f6bc97c44
khaled hossain <khaled.hossain@appnap.io>
2023-05-09 16:29:42 +0600

    Remove Schema

--- routes/items_route.js
+++ routes/items_route.js
@@ -1,10 +1,4 @@
 const {
-  itemsSchema,
-  singleItemSchema,
-  createItemSchema,
-  deleteItemSchema,
-} = require("../model/schema/items");
-const {
   getAllItem,
   getItem,
   postItem,
@@ -12,49 +6,12 @@ const {
 } = require("../controllers/items");
 
 function itemsRoutes(fastify, Option, done) {
-  fastify.get("/items", getAllItemOpts);
-  fastify.post("/items", postItemOpts);
-  fastify.get("/items/:item_id", getItemOpts);
-  fastify.delete("/items/:item_id", deleteItemOpts);
+  fastify.get("/items", {}, getAllItem);
+  fastify.post("/items", {}, postItem);
+  fastify.get("/items/:item_id", {}, getItem);
+  fastify.delete("/items/:item_id", {}, deleteItem);
 
   done();
 }
-
-const getAllItemOpts = {
-  schema: {
-    response: {
-      200: itemsSchema,
-    },
-  },
-  handler: getAllItem,
-};
-
-const getItemOpts = {
-  schema: {
-    response: {
-      200: singleItemSchema,
-    },
-  },
-  handler: getItem,
-};
-
-const postItemOpts = {
-  schema: {
-    body: createItemSchema,
-    response: {
-      201: singleItemSchema,
-    },
-  },
-  handler: postItem,
-};
-
-const deleteItemOpts = {
-  schema: {
-    response: {
-      200: deleteItemSchema,
-    },
-  },
-  handler: deleteItem,
-};
 
 module.exports = itemsRoutes;
